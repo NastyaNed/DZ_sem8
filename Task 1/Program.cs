94d3220f80c94de05e89236c6d3cd682a1bc9503
NastyaNed <acherkaeva1997@gmail.com>
2022-12-09 15:38:21 +0300

    added task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.WriteLine("Введите количество строк массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбов массива:");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int [m, n];
+
+for (int i = 0; i < m; i++) 
+{
+    for (int j = 0; j < n; j++) 
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+for (int i = 0; i < m; i++) 
+{
+    for (int j = 0; j < n; j++) 
+    {
+        int temp = matrix[i,j];
+        for (int z = j+1; z < n; z++)
+        {
+            if (matrix[i,z] > temp)
+            {
+                matrix[i,j] = matrix[i,z];
+                matrix[i,z] = temp;
+                temp = matrix[i,j];
+            }
+
+        }
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
